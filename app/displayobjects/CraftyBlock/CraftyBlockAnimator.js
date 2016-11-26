afd740ed1236fd47b42a434b03166b65c9eebf02
Minwoo Lee <mark8098@gmail.com>
2016-11-27 06:03:14 +0900

    Replace global variables with class properties

--- app/displayobjects/CraftyBlock/CraftyBlockAnimator.js
+++ app/displayobjects/CraftyBlock/CraftyBlockAnimator.js
@@ -2,13 +2,13 @@ import PIXI from 'pixi.js';
 import CraftyBlock from './CraftyBlock.js';
 import CraftyBlockSpec from './CraftyBlockSpec.js';
 
-let IS_DRAGGING = false;
-let MOUSEOVER_BLOCK = null;
-
 var stage;
 
 class CraftyBlockAnimator {
     constructor() {
+        console.log(`DEBUG:::Created CraftyBlockAnimator`);
+        this.isHoldingBlock = false;
+        this.targetBlock = null;
     }
 
     makeInteractive(block) {
@@ -18,7 +18,6 @@ class CraftyBlockAnimator {
 
         //  enable drag and drop for non-parameter blocks, enable mouse over check for parameter blocks
         if (block.blockInfo.type != CraftyBlock.PARAMETER) {
-            console.log(`helloo from ${block.blockInfo.name}`);
             block
                 .on('mousedown', this.onDragStart)
                 .on('touchstart', this.onDragStart)
@@ -42,19 +41,19 @@ class CraftyBlockAnimator {
         block
             .on('clickonce', this.onClick)
     }
+
     onClick() {
         console.log("Clicked!");
         showSetting(this);
     }
 
     onDragStart(event) {
+
         stage = this._getStage();
+
         let relativeMousePosition = event.data.getLocalPosition(this);
         if (this.hitArea.contains(relativeMousePosition.x, relativeMousePosition.y)) {
             //console.log("DEBUG::: drag started by \"" + this.blockInfo.name + "\"");
-
-            //  set toggle that becomes true the moment when drag starts
-            this.startedDragging = false;
 
             //  if parent is sidebar, create copy
             if (this.parent.id == "sidebar") {
@@ -71,71 +70,28 @@ class CraftyBlockAnimator {
             this.originalPosition = new PIXI.Point(this.position.x,this.position.y);
 
             this.alpha = 0.6;
-            this.dragging = true;
-            IS_DRAGGING = true;
+            this.selected = true;
+            CraftyBlockAnimator.isHoldingBlock = true;
+            //  set toggle that becomes true the moment when drag starts
+            this.isClick = true;
 
             $('.delete_btn').remove();
-        }
-    }
-
-    onDragEnd(event) {
-        //  if block had been clicked, check drag or click, and execute by case
-        if (this.dragging) {
-            // case: dragged
-
-            //  if parent is sidebar, either add new Block to stage or remove depending on mouse location
-            if (this.parent.id == "sidebar") {
-                let relativeMousePosition = event.data.getLocalPosition(this.parent);
-                if (this.sidebarRect.contains(relativeMousePosition.x, relativeMousePosition.y)) {
-                    this.parent.removeChild(this);
-                } else {
-                    this.addToStage();
-                }
-            }
-
-            if (this.startedDragging) {
-                //console.log("DEBUG::: drag ended by \"" + this.blockInfo.name + "\"");
-
-                //  if there is parameter block below, attach
-                if (MOUSEOVER_BLOCK) {
-                    this.attachTo(MOUSEOVER_BLOCK);
-                    MOUSEOVER_BLOCK = null;
-                }
-
-                //  render update the taken out block
-                if (this.originalBlock) {
-                    this.originalBlock.update();
-                }
-
-                checkBlockInfoList();
-            }
-            // case: clicked
-            else {
-                this.emit('clickonce');
-                //  export block
-                //  call canvasClicked function
-                // canvasClicked(this.stringify());
-            }
-
-            this.alpha = 1;
-            this.dragging = false;
-            IS_DRAGGING = false;
         }
     }
 
     onDragMove(event) {
         //console.log("DEBUG::: drag moving by \"" + this.blockInfo.name + "\"");
 
-        if (this.dragging)
+        if (this.selected)
         {
-            //  re-position block to relative mouse position
+            //  move block to mouse position
             let newPosition = event.data.getLocalPosition(this.parent);
             this.position.x = newPosition.x - this.diff.x;
             this.position.y = newPosition.y - this.diff.y;
 
-            //  trigger when first drag after select
-            if (!this.startedDragging) {
-                this.startedDragging = true;
+            //  set isClick to false since block started to move
+            if (this.isClick) {
+                this.isClick = false;
 
                 //  if block has parent block, detach from parent block and set originalBlock for later update()
                 if (this.parent.hasOwnProperty('blockInfo')) {
@@ -147,27 +103,67 @@ class CraftyBlockAnimator {
         }
     }
 
-    // "mousemove" event handler for parameter blocks
-    onParameterMove(event) {
-        //  if mouse position is inside hit area, then set stage.target to this
-        if (IS_DRAGGING) {
-            let relativeMousePosition = event.data.getLocalPosition(this);
-            if (this.hitArea.contains(relativeMousePosition.x, relativeMousePosition.y)) {
-                //console.log(`DEBUG::: parameter moving by {${this.blockInfo.name}}`);
-
-                this.getChildAt(0).tint = 0xDDDDDD;
-                MOUSEOVER_BLOCK = this;
-            } else {
-                if (this == MOUSEOVER_BLOCK) {
-                    MOUSEOVER_BLOCK = null;
-                    this.getChildAt(0).tint = 0xFFFFFF;
+    /**
+     * Called when click/drag of a block is ended
+     */
+    onDragEnd(event) {
+        if (this.selected) {
+            //  if parent is sidebar, either add new Block to stage or remove depending on mouse location
+            if (this.parent.id == "sidebar") {
+                let relativeMousePosition = event.data.getLocalPosition(this.parent);
+                if (this.sidebarRect.contains(relativeMousePosition.x, relativeMousePosition.y)) {
+                    this.parent.removeChild(this);
+                } else {
+                    this.addToStage();
                 }
             }
+
+            if (this.isClick) {
+                this.emit('clickonce');
+            } else {
+                //console.log("DEBUG::: drag ended by \"" + this.blockInfo.name + "\"");
+
+                //  if there is parameter block below, attach
+                if (CraftyBlockAnimator.targetBlock) {
+                    this.attachTo(CraftyBlockAnimator.targetBlock);
+                    CraftyBlockAnimator.targetBlock = null;
+                }
+
+                //  render update the taken out block
+                if (this.originalBlock) {
+                    this.originalBlock.update();
+                }
+
+                checkBlockInfoList();
+            }
+
+            this.alpha = 1;
+            this.selected = false;
+            CraftyBlockAnimator.isHoldingBlock = false;
         }
     }
 
     onParameterStart(event) {
         this.clicked = true;
+    }
+
+    // "mousemove" event handler for parameter blocks
+    onParameterMove(event) {
+        //  if mouse position is inside hit area, then set stage.target to this
+        if (CraftyBlockAnimator.isHoldingBlock) {
+            let relativeMousePosition = event.data.getLocalPosition(this);
+            if (this.hitArea.contains(relativeMousePosition.x, relativeMousePosition.y)) {
+                //console.log(`DEBUG::: parameter moving by {${this.blockInfo.name}}`);
+
+                this.getChildAt(0).tint = 0xDDDDDD;
+                CraftyBlockAnimator.targetBlock = this;
+            } else {
+                if (this == CraftyBlockAnimator.targetBlock) {
+                    CraftyBlockAnimator.targetBlock = null;
+                    this.getChildAt(0).tint = 0xFFFFFF;
+                }
+            }
+        }
     }
 
     onParameterEnd(event) {
