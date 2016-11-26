45e97eb75a7a42d634e379fa960bef46a7e18a13
Minwoo Lee <mark8098@gmail.com>
2016-11-27 08:24:10 +0900

    Add functionWithName function

--- app/displayobjects/CraftyBlock/CraftyBlockSpec.js
+++ app/displayobjects/CraftyBlock/CraftyBlockSpec.js
@@ -1,3 +1,6 @@
+import * as PASTEL_FUNC from '../../functions/functions.js';
+import CraftyBlock from './CraftyBlock.js';
+
 export default class CraftyBlockSpec {
     constructor(name, type, parameters = [], library="", docstring="") {
         this.name = name;
@@ -5,5 +8,13 @@ export default class CraftyBlockSpec {
         this.parameters = parameters;
         this.docstring = docstring;
         this.library = library;
+    }
+
+    static functionWithName(name) {
+        let info = [ ...PASTEL_FUNC.STANDARD, ...PASTEL_FUNC.MATH].find( functionInfo => { 
+            return functionInfo.name === name; 
+        });
+
+        return new CraftyBlockSpec(info.name, CraftyBlock.FUNCTION, info.parameters, "", info.docstring);
     }
 }
