d03ef8b0827b081b736fd11dc74f3fca55e94d90
Minwoo Lee <mark8098@gmail.com>
2016-11-27 06:01:51 +0900

    Correct Indent

--- app/displayobjects/App/App.js
+++ app/displayobjects/App/App.js
@@ -10,46 +10,46 @@ import Sidebar from '../Sidebar/Sidebar.js';
  * @extends ScaledContainer
  */
 export default class App extends PIXI.Container {
-  constructor(...args) {
-    super();
+    constructor(...args) {
+        super();
 
-    var stage = this;
-    stage.id = "stage";
-    stage.interactive = false;
+        var stage = this;
+        stage.id = "stage";
+        stage.interactive = false;
 
-    let bg = new Background();
-    let sidebar = new Sidebar();
+        let bg = new Background();
+        let sidebar = new Sidebar();
 
-    stage.addChild(bg);
+        stage.addChild(bg);
 
-    var open_palette_btn = $('<input class="open-palette" type="button" value="+"/>');
-    open_palette_btn.css('width', 50);
-    open_palette_btn.css('height', 50);
-    open_palette_btn.css('position', 'absolute');
-    open_palette_btn.css('top', 50);
-    open_palette_btn.css('left', 100);
-    open_palette_btn.css('border', 'none');
-    open_palette_btn.css('background-color', '#92A8D1');
-    open_palette_btn.css('border-radius', 25);
-    open_palette_btn.css('font-size', 24);
-    open_palette_btn.css('font-weight', 'bold');
-    open_palette_btn.css('color', '#333333');
-    open_palette_btn.css('border-radius', 25);
-    $("body").append(open_palette_btn);
+        var open_palette_btn = $('<input class="open-palette" type="button" value="+"/>');
+        open_palette_btn.css('width', 50);
+        open_palette_btn.css('height', 50);
+        open_palette_btn.css('position', 'absolute');
+        open_palette_btn.css('top', 50);
+        open_palette_btn.css('left', 100);
+        open_palette_btn.css('border', 'none');
+        open_palette_btn.css('background-color', '#92A8D1');
+        open_palette_btn.css('border-radius', 25);
+        open_palette_btn.css('font-size', 24);
+        open_palette_btn.css('font-weight', 'bold');
+        open_palette_btn.css('color', '#333333');
+        open_palette_btn.css('border-radius', 25);
+        $("body").append(open_palette_btn);
 
-    $('.open-palette').click(function() {
-      if(stage.children.length == 1) {
-        stage.addChild(sidebar);
-        open_palette_btn.val('-');
-      } else {
-        if(stage.getChildAt(1).id == "sidebar") {
-          stage.removeChild(sidebar);
-          open_palette_btn.val('+');
-        } else {
-          stage.addChildAt(sidebar, 1);
-          open_palette_btn.val('-');
-        }
-      }
-    });
-  }
+        $('.open-palette').click(function() {
+            if(stage.children.length == 1) {
+                stage.addChild(sidebar);
+                open_palette_btn.val('-');
+            } else {
+                if(stage.getChildAt(1).id == "sidebar") {
+                    stage.removeChild(sidebar);
+                    open_palette_btn.val('+');
+                } else {
+                    stage.addChildAt(sidebar, 1);
+                    open_palette_btn.val('-');
+                }
+            }
+        });
+    }
 }
